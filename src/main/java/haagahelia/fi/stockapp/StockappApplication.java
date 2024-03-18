f9c9fb9d6bf0c8e711443a39b51214f0559eee0d
janphan <janphan061@gmail.com>
2024-03-18 12:04:35 +0200

    Add 2 more stocks

--- src/main/java/haagahelia/fi/stockapp/StockappApplication.java
+++ src/main/java/haagahelia/fi/stockapp/StockappApplication.java
@@ -30,11 +30,15 @@ public class StockappApplication {
 			log.info("save a couple of stocks");
 			// save some categories
 			crepository.save(new Category("Dry"));
-			crepository.save(new Category("Frozen"));
+			crepository.save(new Category("Medicine"));
 			crepository.save(new Category("Spice"));
 			// save stocks
 			srepository.save(new Stock("Rice", "20.06.2024", 25, "Thailan premium rice", 40,
 					crepository.findByName("Dry").get(0)));
+			srepository.save(new Stock("Fish sauce", "15.06.2025", 1, "Squid fish sauce", 2.56,
+					crepository.findByName("Spice").get(0)));
+			srepository.save(new Stock("Vitamin B12", "04.07.2025", 1, "Vitamin B12 - VitaTabs 1000microgram", 7.99,
+					crepository.findByName("Medicine").get(0)));
 
 			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6",
 					"user1@gmail.com", "USER");
